a2205c5f2e3b600be9e5d91b523ddb47d0c345a3
hqj <185138375@qq.com>
2019-08-10 12:18:49 +0800

    2019-08-10

---
+++ myProject/js/myjs/hello.js
@@ -0,0 +1,10 @@
+// 外部资源
+var a = "我是'外部'js资源的";
+alert(a);
+// alert(typeof a);//检测数据类型的一元运算符，并且返回的结果始终是一个字符串。
+
+switch (2) {
+    case 1:
+        alert("is ")
+        break;
+}
---
+++ myProject/js/myjs/数据类型.js
@@ -0,0 +1,15 @@
+// //最大值
+// var a = Number.MAX_VALUE;
+// //最小值
+// var b = Number.MIN_VALUE;
+//
+// //无穷大
+// alert("无穷大" + Number.POSITIVE_INFINITY === Infinity);
+// //无穷小
+// alert("无穷小" + Number.NEGATIVE_INFINITY === -Infinity);
+
+//当一个字符串，另一个是数值的时候，把字符串尝试转换成数值类型，然后进行比较，
+//如果不能转换成数值类型，则会返回NaN,然后返回假
+var a="123";
+var b=1;
+alert(a>b);
